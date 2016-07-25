b4188777da6de71d0f091aa1e40ee84b8aa65d48
George Richardson <g.raymond.richardson@gmail.com>
2016-07-25 16:40:23 -0700

    Completed analysis class

--- bin/main.py
+++ bin/main.py
@@ -13,7 +13,7 @@ class Manager(object):
 		filepath = os.path.expanduser('~/projects/ch1_sea_dl_speeds/data/'+self.filepath)
 		data = pd.read_csv(filepath)
 		return self.data
-
+		
 	def clean_advt_spd(self):
 		data = self.data[data.advertised_download.notnull()]
 		data = data[data.advertised_download != 0]
@@ -23,8 +23,8 @@ class Manager(object):
 		data = self.data[data.cost_of_service != dont_know]
 		return data_cln_cost
 
-	def select_isp(self, data=None, isp=None):
-		data_isp = data[data.isp_user == isp]
+	def select_isp(self, data, isp=None):
+		data_isp = self.data[self.data.isp_user == isp]
 		return data_isp
 
 
@@ -32,15 +32,15 @@ class Analyser(object):
 
     def __init__(self, manager):
         self.manager = manager
+        self.data = manager.data
 
-	def cost_spd(data):
+	def cost_spd(self, data):
 		grouped = data['actual_download'].groupby(data['cost_of_service'])
 		grouped_stats = grouped.describe()
 		box = grouped.boxplot()	
-		return grouped
+		return grouped, box, grouped_stats
 
-	def percent_spd_isp(spd_data):
-		spd_data = clean_advt_spd(spd_data)
+	def percent_spd(self, spd_data):
 	
 		act_dl = spd_data.actual_download
 		adv_dl = spd_data.advertised_download
@@ -52,26 +52,43 @@ class Analyser(object):
 		binned = pd.cut(percent_dl, bins)
 	
 		percent_dl_binned = pd.value_counts(binned).sort_index()
-		percent_dl_binned = (percent_dl_binned / sum(percent_dl_binned)) * 100    
+		percent_dl_binned = (percent_dl_binned / sum(percent_dl_binned)) * 100
+		histogram = percent_dl_binnied.hist()   
 	
 		mean_percent_dl = percent_dl.mean()
 	
-		return percent_dl_binned, mean_percent_dl
+		return percent_dl_binned, histogram, mean_percent_dl
 
 	
 	def analyse_isp(filepath, isp=None):
-		df = read_spd(filepath)
-		df = select_isp(df, isp=isp)
+		data = self.manager.select_isp(df, isp=isp)
 	
 		percent_dl_binned, mean_percent_dl = percent_spd_isp(df)
 	
 		return percent_dl_binned, mean_percent_dl
+
+	def full_analysis(self, isps):
+	    
+	    data_cost = self.manager.clean_cost(dont_know='dont_know')
+	    data_spd = self.manager.clean_advt_spd()
+	    
+	    for isp in isps:
+	        data_cost_isp = self.manager.select_isp(data_cost, isp=isp)
+	        grouped, box, grouped_stats = self.manager.cost_spd(data_cost_isp)
+	        cost_dict[isp]={grouped, box, grouped_stats}
+	        
+	        data_spd_isp = self.manager.select_isp(data_spd, isp=isp)
+	        binned_dl, histogram, mean_dl = percent_spd(data_spd)
+	        spd_dict[isp]={binned_dl, histogram, mean_dl}
+	        
+	        return spd_dict, cost_dict
 
 class Plotter(object): 
 
 	def full_plot(data):
 		pass
 
+isps = ['comcast','centurylink','wave']
 
 data = read_spd('sea_broadband_dl_speed_test_2016.csv')
 data, grouped = clean_cost(data, dont_know='dont_know')
