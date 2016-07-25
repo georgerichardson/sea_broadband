c467fb330c3fe9c593179f6211afec1e4fbef18f
George Richardson <g.raymond.richardson@gmail.com>
2016-07-25 13:07:11 -0700

    Completely class functionality in Manager()

--- bin/main.py
+++ bin/main.py
@@ -4,28 +4,38 @@ import matplotlib.pyplot as plt
 import os
 
 class Manager(object):
-	def read_spd(filepath):
-		filepath = os.path.expanduser('~/projects/ch1_sea_dl_speeds/data/'+filepath)
-		df = pd.read_csv(filepath)
-		return df
 
-	def clean_advt_spd(spd_data):
-		spd_data = spd_data[spd_data.advertised_download.notnull()]
-		spd_data = spd_data[spd_data.advertised_download != 0]
-		return spd_data
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+	def read_spd(self):
+		filepath = os.path.expanduser('~/projects/ch1_sea_dl_speeds/data/'+self.filepath)
+		data = pd.read_csv(filepath)
+		return self.data
+
+	def clean_advt_spd(self):
+		data = self.data[data.advertised_download.notnull()]
+		data = data[data.advertised_download != 0]
+		return data_cln_spd
 	 
-	def clean_cost(spd_data, dont_know=None):
-		spd_data = spd_data[spd_data.cost_of_service != dont_know]
-		return spd_data, grouped
+	def clean_cost(self, dont_know=None):
+		data = self.data[data.cost_of_service != dont_know]
+		return data_cln_cost
 
-	def select_isp(spd_data, isp=None):
-		select_isp = spd_data[spd_data.isp_user == isp]
-		return select_isp
+	def select_isp(self, data=None, isp=None):
+		data_isp = data[data.isp_user == isp]
+		return data_isp
 
 
 class Analyser(object):
+
+    def __init__(self, manager):
+        self.manager = manager
+
 	def cost_spd(data):
-		grouped = spd_data['actual_download'].groupby(spd_data['cost_of_service'])
+		grouped = data['actual_download'].groupby(data['cost_of_service'])
+		grouped_stats = grouped.describe()
+		box = grouped.boxplot()	
 		return grouped
 
 	def percent_spd_isp(spd_data):
@@ -57,13 +67,14 @@ class Analyser(object):
 		return percent_dl_binned, mean_percent_dl
 
 class Plotter(object): 
+
 	def full_plot(data):
 		pass
+
 
 data = read_spd('sea_broadband_dl_speed_test_2016.csv')
 data, grouped = clean_cost(data, dont_know='dont_know')
 print data.head(10)
-print '-------------------------------------'
 print grouped
     
     
